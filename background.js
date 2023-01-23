88f29f483ed80c4a3271c04469ef9c37ed67699e
Jean-Baptiste <jeanbaptiste.bertrand@gmail.com>
2023-01-23 22:49:46 +0100

    v 1.4
    - added support for other wikimedia projects: Wikimedia Commons, Wiktionary, Wikisource, Wikidata, and MediaWiki
    - making an exception for https://nostalgia.wikipedia.org, and partially for mobile versions of Wikimedia projects.
    - allows to manually override the "useskin=vector" parameter

--- background.js
+++ background.js
@@ -1,28 +1,29 @@
 var field = 'useskin';
 var value = "vector";
-const excludedUrls = [/www[.]wikipedia[.]org/];
+//let reMobile = /([.]m[.]|^m[.])/ //something to use for future add-on versions
+const excludedUrls = [/(www|nostalgia|[.]m)[.]wikipedia[.]org/, /www[.]wiktionary[.]org/,
+/[:]\/\/(.*[.]m[.]|m[.])/, /mobileaction=toggle_view_mobile/]
 
-
-function redirect(requestDetails) {
-  var isMatch = excludedUrls.some(function(rx) { return rx.test(requestDetails.url); });
-  if (isMatch) {
-		return requestDetails.url;		
+function redirect(requestDetails) {	
+  currentUrl = requestDetails.url;  
+  var url = new URL(currentUrl);  	  
+  var isMatch = excludedUrls.some(function(rx) { return rx.test(currentUrl); });
+  if (isMatch) {	 		
+		return currentUrl;			  
+  }	
+  if (url.searchParams.has(field)) {		 
+		return currentUrl;		
+  } else {		
+		url.searchParams.set(field, value);		
+		return {
+			redirectUrl: url.toString()
+		}; 
   }
-  if(requestDetails.url.indexOf('?' + field + '=' + value) != -1) {		
-		return requestDetails.url;		
-	} else if(requestDetails.url.indexOf('&' + field + '=' + value) != -1) {
-		return requestDetails.url;
-	} else {
-	const url = new URL(requestDetails.url);
-	url.searchParams.set(field, value);
-	return {
-    redirectUrl: url.toString()
-  }; 
-	}
 }
 
 browser.webRequest.onBeforeRequest.addListener(
   redirect,
-  {urls:["*://*.wikipedia.org/*"], types:["main_frame"]},
+  {urls:["*://*.wikipedia.org/*", "*://*.wiktionary.org/*", "*://commons.wikimedia.org/*", 
+  "*://*.wikisource.org/*",  "*://*.wikidata.org/*", "*://*.mediawiki.org/*" ], types:["main_frame"]},
   ["blocking"]
 );
